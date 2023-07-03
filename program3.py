f3ebfa96ba29a7179bda0281e4bde1c4bfcf7bc5
jalish chauhan <62424952+jalishchauhan07@users.noreply.github.com>
2023-07-03 23:00:10 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,9 @@
+x=int(input("Enter No:"),10);
+temp=1;
+for i in range(0,x):
+    if(temp=1 or temp==2):
+        print(1,end=" ")
+    else:
+        s=temp+i
+        print(s,end=" ")
+    temp=temp+1
